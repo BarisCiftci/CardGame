804679c3927dd2eee154c5dec6a9e5ba3723881f
Baris Ciftci <barishciftci@gmail.com>
2023-03-28 22:28:58 +0200

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Baris Ciftci on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

4cea5b9cc939349a3eb5319d50939834f0763dba
Baris Ciftci <barishciftci@gmail.com>
2023-05-01 21:33:51 +0200

    NO_TICKET: Remove background colour constants form "Constants" file

--- CardGame/Constants.swift
+++ CardGame/Constants.swift
@@ -9,17 +9,6 @@ import Foundation
 import SwiftUI
 import AVFAudio
 
-// Background gradient colors
-let lightGreenBackground: Color = Color(
-    red: 12/255,
-    green: 119/255,
-    blue: 0/255)
-
-let darkGreenBackground: Color = Color(
-    red: 8/255,
-    green: 68/255,
-    blue: 0/255)
-
 let playerNameUser = "Human"
 let playerNameCpu = "ChatGPT"
 let player = try! AVAudioPlayer(contentsOf: URL(fileURLWithPath: Bundle.main.path(forResource: "sound", ofType: "mp3")!))
